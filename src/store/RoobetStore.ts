6fa9b382dd673e28dc2be86e8cd3e12db100c874
Skandeerkefi <skanderkefi88@gmail.com>
2025-08-20 11:13:46 +0100

    WIP: Save progress before merging into master

--- src/store/RoobetStore.ts
+++ src/store/RoobetStore.ts
@@ -32,7 +32,8 @@ export const useRoobetStore = create<RoobetStore>((set) => ({
 		set({ loading: true, error: null });
 
 		try {
-			let url = "/api/leaderboard";
+			let url =
+				"https://misterteedata-production.up.railway.app/api/leaderboard";
 			if (startDate && endDate) {
 				url += `/${startDate}/${endDate}`;
 			}
